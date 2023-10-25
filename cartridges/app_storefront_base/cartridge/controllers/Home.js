b95090be5ffbe2b64221ef331a99c9ece76e125a
Karolina Rachuta <karolina.rachuta@gmail.com>
2023-10-25 16:08:04 +0400

    small changes

--- cartridges/app_storefront_base/cartridge/controllers/Home.js
+++ cartridges/app_storefront_base/cartridge/controllers/Home.js
@@ -10,7 +10,7 @@ var consentTracking = require('*/cartridge/scripts/middleware/consentTracking');
 var pageMetaData = require('*/cartridge/scripts/middleware/pageMetaData');
 
 /**
- * Any customization on this endpoint3, also requires update for Default-Start endpoint
+ * Any customization on this endpoint, also requires update for Default-Start endpoint
  */
 /**
  * Home-Show : This endpoint is called when a shopper navigates to the home page
