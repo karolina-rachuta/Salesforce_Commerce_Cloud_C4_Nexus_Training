e018503b010d9a8c4369f20f38d90f48d9317fff
Karolina Rachuta <karolina.rachuta@gmail.com>
2023-10-25 16:37:06 +0400

    after the merge

--- cartridges/app_storefront_base/cartridge/controllers/Home.js
+++ cartridges/app_storefront_base/cartridge/controllers/Home.js
@@ -8,10 +8,9 @@ var server = require('server');
 var cache = require('*/cartridge/scripts/middleware/cache');
 var consentTracking = require('*/cartridge/scripts/middleware/consentTracking');
 var pageMetaData = require('*/cartridge/scripts/middleware/pageMetaData');
-var userLoggedIn = require('*/cartridge/scripts/middleware/userLoggedIn');
 
 /**
- * Any customization on this endpoint, also requires update for Default-Start endpoint
+ * Any customization on this endpoint3, also requires update for Default-Start endpoint
  */
 /**
  * Home-Show : This endpoint is called when a shopper navigates to the home page
@@ -24,26 +23,22 @@ var userLoggedIn = require('*/cartridge/scripts/middleware/userLoggedIn');
  * @param {renders} - isml
  * @param {serverfunction} - get
  */
-server.get('Show', userLoggedIn.validateLoggedIn, consentTracking.consent, cache.applyDefaultCache, function (req, res, next) {
+server.get('Show', consentTracking.consent, cache.applyDefaultCache, function (req, res, next) {
     var Site = require('dw/system/Site');
     var PageMgr = require('dw/experience/PageMgr');
     var pageMetaHelper = require('*/cartridge/scripts/helpers/pageMetaHelper');
 
     pageMetaHelper.setPageMetaTags(req.pageMetaData, Site.current);
 
+    var page = PageMgr.getPage('homepage');
 
-        var page = PageMgr.getPage('homepage');
-
-        if (page && page.isVisible()) {
-            res.page('homepage');
-        } else {
-            res.render('home/homePage');
-        }
-
-        next();
-
+    if (page && page.isVisible()) {
+        res.page('homepage');
+    } else {
+        res.render('home/homePage');
+    }
+    next();
 }, pageMetaData.computedPageMetaData);
-
 
 server.get('ErrorNotFound', function (req, res, next) {
     res.setStatusCode(404);
